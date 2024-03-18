c2a2877544eaa8b91e7fc2b954519bf14f6eb30c
TachiZakariae <zakariaetachi@gmail.com>
2024-03-17 17:01:51 -0700

    C - printf

--- main.h
+++ main.h
@@ -10,17 +10,18 @@
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+
 #define NULL_STRING "(null)"
 
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-#define CONVERT_LOWERCASE 1
-#define CONVERT_UNSIGNED 2
+#define CONVERT_LOWERCASE   1
+#define CONVERT_UNSIGNED    2
 
 /**
  * struct parameters - parameters struct
  *
- * @unsign: flaf it unsigned value
+ * @unsign: flag it unsigned value
  *
  * @plus_flag: on if plus_flag specified
  * @space_flag: on if hashtag_flag specified
@@ -85,7 +86,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
 int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
-int *get_width(char *s, params_t *params, va_list ap);
+char *get_width(char *s, params_t *params, va_list ap);
 
 /* convert_number.c module */
 int print_hex(va_list ap, params_t *params);
